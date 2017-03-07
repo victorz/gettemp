96626a684e8275fbc24fec2fb5872a561f850064
Victor Zamanian <victor.zamanian@gmail.com>
2017-03-07 15:56:00 +0100

    Added wind chill.

--- gettemp.go
+++ gettemp.go
@@ -7,15 +7,35 @@ import (
     "strings"
     "strconv"
     "fmt"
+    "math"
 )
+
+func convprep(str string) (out string) {
+    out = strings.Replace(str, ",", ".", 1)
+    out = strings.Trim(out, " ")
+    return
+}
+
+// Convert HTML entity angle brackets to actual angle brackets
+func formatXml(data string) (xml string) {
+    xml = strings.Replace(data, "&lt;", "<", -1)
+    xml = strings.Replace(xml, "&gt;", ">", -1)
+    return
+}
+
+// Implements the standard Wind Chill formula for Environment Canada.
+// Values taken from http://www8.tfe.umu.se/weather-new/js/index.js.
+func windChill(T, V float64) float64 {
+    vtmp := math.Pow(V, 0.16)
+    return 13.126667 + 0.6215 * T + vtmp * (0.4875195 * T - 13.924748)
+}
 
 func main() {
 
     type WeatherData struct {
         Temperature string `xml:"root>tempmed"`
+        WindSpeed string `xml:"root>vindh"`
     }
-
-    v := WeatherData{ Temperature: "none" }
 
     // Weather data URL
     const url = "http://www8.tfe.umu.se/WeatherWebService2012/Service.asmx/Aktuellavarden"
@@ -27,19 +47,21 @@ func main() {
     bytes, _ := ioutil.ReadAll(res.Body)
     res.Body.Close()
 
-    // Convert HTML entity angle brackets to actual angle brackets
-    xmlStr := strings.Replace(string(bytes), "&lt;", "<", -1)
-    xmlStr = strings.Replace(xmlStr, "&gt;", ">", -1)
+    xmlStr := formatXml(string(bytes));
 
     // Parse XML
+    v := WeatherData{ Temperature: "none" }
     xml.Unmarshal([]byte(xmlStr), &v)
 
     // Prepare for conversion
-    tempStr := strings.Replace(v.Temperature, ",", ".", 1)
-    tempStr = strings.Trim(tempStr, " ")
+    tempStr := convprep(v.Temperature)
+    windStr := convprep(v.WindSpeed)
 
     temp, _ := strconv.ParseFloat(tempStr, 32)
+    wind, _ := strconv.ParseFloat(windStr, 32)
+
+    Twc := windChill(temp, wind)
 
     // Output
-    fmt.Printf("Current temp: %.1f°C\n", temp)
+    fmt.Printf("Current temp: %.1f°C\nFeels like: %.1f°C\n", temp, Twc)
 }
